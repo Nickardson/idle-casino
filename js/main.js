d81f52fbed818341257f3a70c8309b9636662483
Nickardson <taylorgratzer@yahoo.com>
2014-12-12 12:10:35 -0500

    Basic currency with big.js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,34 @@ requirejs.config({
         data: "../data",
         text: 'require/text',
         json: 'require/json',
-        async: 'require/async'
+        async: 'require/async',
+        big: 'big.min'
     }
 });
 
-require([], function () {
-	
+require(['big'], function (Big) {
+	var cash = new Big(0);
+
+    function tick(ticks) {
+        // sample income
+        cash = cash.plus(ticks);
+        
+        document.title = cash.toFixed(1);
+    }
+
+    var startTick = new Date();
+    var accumulated = 0;
+    var tickSize = 100;
+    setInterval(function () {
+        var now = new Date();
+        var dt = now - startTick;
+        startTick = now;
+
+        accumulated += dt;
+
+        var ticks = Math.floor(accumulated / tickSize);
+        accumulated %= tickSize;
+
+        tick(ticks * (tickSize / 1000));
+    }, tickSize);
 });
